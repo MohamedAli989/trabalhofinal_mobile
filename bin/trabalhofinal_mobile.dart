2dfa497e2b93b41d221b3c00aaaa5a852c97cfcb
Mohamed Ali <mohamedtoulin@gmail.com>
2025-08-31 18:29:06 -0300

    feat: Initial project structure from dart create

---
+++ bin/trabalhofinal_mobile.dart
@@ -0,0 +1,5 @@
+import 'package:trabalhofinal_mobile/trabalhofinal_mobile.dart' as trabalhofinal_mobile;
+
+void main(List<String> arguments) {
+  print('Hello world: ${trabalhofinal_mobile.calculate()}!');
+}
---
+++ test/trabalhofinal_mobile_test.dart
@@ -0,0 +1,8 @@
+import 'package:trabalhofinal_mobile/trabalhofinal_mobile.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
